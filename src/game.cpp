6769f67640c33c71bcfebf003686bce20e44996c
Denis <denismiskovsky@gmail.com>
2021-07-21 14:11:10 +0200

    Added jumping

--- src/game.cpp
+++ src/game.cpp
@@ -21,53 +21,65 @@ char level[levelHeight][levelWidth] = {
 	{'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'},
 	{'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'}};
 
-int direction;
 const int movingUp = 0;
 const int movingRight = 1;
 const int movingDown = 2;
 const int movingLeft = 3;
-bool isMoving;
-int charSpeed = 100;
-int charPosX;
-int charPosY;
-SDL_Rect playerDst = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, charSize * 2, charSize * 2};
+int maxHeight;
+
+struct Player
+{
+	bool isMoving;
+	bool isJumping;
+	bool isFalling;
+	int direction;
+	int speed = 300;
+	SDL_Rect dst;
+};
+Player player;
 
 void startGame()
 {
-	direction = movingRight;
-	bool isMoving = false;
+	player.direction = movingRight;
+	player.isMoving = false;
 
-	initSpriteAnimation(&moving, sheet, movingFirstFrame, 3, 5);
+	player.dst = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, charSize * 2, charSize * 2};
+	initSpriteAnimation(&moving, sheet, movingFirstFrame, 3, 7);
 }
 
 void movement(float dt)
 {
 	if (keyStates[SDL_SCANCODE_A])
 	{
-		if (direction != movingLeft)
+		if (player.direction != movingLeft)
 		{
 			resetAnimation(&moving);
-			direction = movingLeft;
+			player.direction = movingLeft;
 		}
-		playerDst.x -= charSpeed * dt;
-		isMoving = true;
+		player.dst.x -= player.speed * dt;
+		player.isMoving = true;
 		updateAnimation(&moving, dt);
 		return;
 	}
 	if (keyStates[SDL_SCANCODE_D])
 	{
-		if (direction != movingRight)
+		if (player.direction != movingRight)
 		{
 			resetAnimation(&moving);
-			direction = movingRight;
+			player.direction = movingRight;
 		}
-		playerDst.x += charSpeed * dt;
-		isMoving = true;
+		player.dst.x += player.speed * dt;
+		player.isMoving = true;
 		updateAnimation(&moving, dt);
 		return;
 	}
+	if (keyStates[SDL_SCANCODE_SPACE] && !lastKeyStates[SDL_SCANCODE_SPACE] && !player.isJumping && !player.isFalling)
+	{
+		player.isJumping = true;
+		maxHeight = player.dst.y - 100;
+	}
 
-	isMoving = false;
+	player.isMoving = false;
 	resetAnimation(&moving);
 }
 
@@ -80,19 +92,29 @@ void update(float dt)
 
 	movement(dt);
 
-	// falling
-	SDL_Rect updatedPlayer = playerDst;
+	// player.isFalling
+	SDL_Rect updatedPlayer = player.dst;
 	updatedPlayer.y++;
-	bool falling = true;
-	for (int row = 0; row < levelHeight; row++)
-		for (int col = 0; col < levelWidth; col++)
-			if (SDL_HasIntersection(&updatedPlayer, &walkable[row][col]))
-			{
-				falling = false;
-				break;
-			}
-	if (falling)
-		playerDst.y += 5;
+	player.isFalling = true;
+	if (!player.isJumping)
+	{
+		for (int row = 0; row < levelHeight; row++)
+			for (int col = 0; col < levelWidth; col++)
+				if (SDL_HasIntersection(&updatedPlayer, &walkable[row][col]))
+				{
+					player.isFalling = false;
+					break;
+				}
+		if (player.isFalling)
+			player.dst.y += 8;
+	}
+
+	if (player.isJumping)
+	{
+		player.dst.y -= 8;
+		if (player.dst.y <= maxHeight)
+			player.isJumping = false;
+	}
 }
 
 void draw(float dt)
@@ -120,7 +142,7 @@ void draw(float dt)
 		}
 	}
 
-	drawAnimatedSprite(renderer, &moving, playerDst);
+	drawAnimatedSprite(renderer, &moving, player.dst);
 }
 void endGame()
 {
