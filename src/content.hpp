6707af9249950711cdbf815ede3f7a8e9dd01091
Denis <denismiskovsky@gmail.com>
2021-07-21 23:46:22 +0200

    Added tile srcs

--- src/content.hpp
+++ src/content.hpp
@@ -27,5 +27,15 @@ bool loadContent()
 SDL_Rect fullScreen = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
 
 // tilesSheet
+SDL_Rect grassLeftSrc = {0, 0, 16, 16 };
 SDL_Rect grassMiddleSrc = { 16, 0, 16, 16 };
+SDL_Rect grassRightSrc = { 32, 0 , 16, 16 };
+SDL_Rect dirtLeftSrc = { 0, 16, 16, 16 };
 SDL_Rect dirtSrc = { 16, 16, 16, 16 };
+SDL_Rect dirtRightSrc = { 32, 16, 16, 16 };
+SDL_Rect dirtBotLeftSrc = { 0, 32, 16, 16 };
+SDL_Rect dirtBotSrc = { 16, 32, 16, 16 };
+SDL_Rect dirtBotRightSrc = { 32, 32, 16, 16 };
+SDL_Rect rockSrc = { 16, 64, 16, 16 };
+SDL_Rect rock2Src = { 16, 80 , 16, 16 };
+SDL_Rect bushSrc = { 16, 48, 16, 16 };
