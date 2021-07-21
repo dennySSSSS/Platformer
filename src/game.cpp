efb44cbc2aa48668de5fd512eb64444278a2f2c1
Denis <denismiskovsky@gmail.com>
2021-07-21 23:46:35 +0200

    New level design

--- src/game.cpp
+++ src/game.cpp
@@ -12,14 +12,14 @@ char level[levelHeight][levelWidth] = {
 	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
 	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
 	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+	{'0', 'G', 'G', 'G', 'G', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
 	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+	{'0', 'G', 'G', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+	{'0', 'D', 'D', 'G', 'G', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+	{'0', 'D', 'D', 'D', 'D', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
 	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
-	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
-	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
-	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
-	{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
-	{'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'},
-	{'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'}};
+	{'0', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', '0'},
+	{'0', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', '0'}};
 
 const int movingUp = 0;
 const int movingRight = 1;
@@ -52,7 +52,7 @@ void movement(float dt)
 	if (keyStates[SDL_SCANCODE_SPACE] && !player.isJumping && !player.isFalling)
 	{
 		player.isJumping = true;
-		maxHeight = player.dst.y - 100;
+		maxHeight = player.dst.y - 250;
 	}
 	if (keyStates[SDL_SCANCODE_A])
 	{
@@ -78,7 +78,6 @@ void movement(float dt)
 		updateAnimation(&moving, dt);
 		return;
 	}
-	
 
 	player.isMoving = false;
 	resetAnimation(&moving);
@@ -111,7 +110,7 @@ void update(float dt)
 	// Jumping
 	if (player.isJumping)
 	{
-		player.dst.y -= 8;
+		player.dst.y -= 15;
 		if (player.dst.y <= maxHeight)
 			player.isJumping = false;
 	}
@@ -131,13 +130,33 @@ void draw(float dt)
 			SDL_Rect dst = {col * tileSize, row * tileSize, tileSize, tileSize};
 			if (level[row][col] == 'D')
 			{
-				drawSprite(renderer, sheet, dirtSrc, dst);
+				// drawSprite(renderer, sheet, dirtSrc, dst);
 				walkable[row][col] = dst;
+
+				if (level[row][col + 1] == 'D' && level[row][col - 1] == 'D' && level[row+1][col] == '0')
+					drawSprite(renderer, sheet, dirtBotSrc, dst);						// dirt bottom
+				else if (level[row + 1][col] == '0' && level[row][col + 1] == '0')
+					drawSprite(renderer, sheet, dirtBotRightSrc, dst);					// dirt bottom right
+				else if (level[row + 1][col] == '0' && level[row][col - 1] == '0')
+					drawSprite(renderer, sheet, dirtBotLeftSrc, dst);					// dirt bottom left
+				else if (level[row][col + 1] == '0' && level[row + 1][col] == 'D')
+					drawSprite(renderer, sheet, dirtRightSrc, dst);						// dir right
+				else if (level[row][col - 1] == '0' && level[row + 1][col] == 'D')
+					drawSprite(renderer, sheet, dirtLeftSrc, dst);						// dirt left
+				else
+					drawSprite(renderer, sheet, dirtSrc, dst);							// dirt middle
 			}
 			else if (level[row][col] == 'G')
 			{
-				drawSprite(renderer, sheet, grassMiddleSrc, dst);
+				// drawSprite(renderer, sheet, grassMiddleSrc, dst);
 				walkable[row][col] = dst;
+
+				if (level[row][col + 1] == '0')
+					drawSprite(renderer, sheet, grassRightSrc, dst);
+				else if (level[row][col - 1] == '0')
+					drawSprite(renderer, sheet, grassLeftSrc, dst);
+				else
+					drawSprite(renderer, sheet, grassMiddleSrc, dst);
 			}
 		}
 	}
