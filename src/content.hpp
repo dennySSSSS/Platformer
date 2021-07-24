b45a85204ebd756c3f20bf64e3041fbd3493d1ba
Denis <denismiskovsky@gmail.com>
2021-07-25 00:26:14 +0200

    Moved struct Player

--- src/content.hpp
+++ src/content.hpp
@@ -7,22 +7,22 @@
 #include "game.hpp"
 // #include "level.hpp"
 
+struct Player
+{
+	bool isMoving;
+	bool isJumping;
+	bool isFalling;
+	int direction;
+	int speed = 250;
+	SDL_Rect dst;
+};
+
 // Sheet
 const int charSize = 16;
 Sprite* sheet = NULL;
 
 SpriteAnimation moving;
 SDL_Rect movingFirstFrame = { 4 * charSize, 5 * charSize, charSize, charSize };
-
-bool loadContent()
-{
-	sheet = loadSprite(renderer, "assets/tileset_16x16.png");
-
-	if (sheet)
-		return true;
-	return false;
-}
-
 
 SDL_Rect fullScreen = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
 
